0c767ce749dd85062813438ed91ff58002c0388c
mrbo10890 <66269205+mrbo10890@users.noreply.github.com>
2020-06-26 19:50:49 +0700

    exam

---
+++ demo/demo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  demo
+//
+//  Created by MacBook on 6/5/20.
+//  Copyright © 2020 Lê Nam. All rights reserved.
+//
+
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    printf("nhập a=\n");
+    scantf("%d" ,&a);
+    printf("nhập b=\n");
+}
